d610413722455d1aaa33009e4c3ee022dd1e5226
Sommelier <Arty.03.07@gmail.com>
2026-05-22 11:37:39 +0300

    chore(release): sync generated build version

--- backend/internal/buildinfo/version_generated.go
+++ backend/internal/buildinfo/version_generated.go
@@ -1,7 +1,7 @@
 package buildinfo
 
 const (
-	generatedVersion = "0.3.0-dev.4+g1fe508be7ea0"
-	generatedCommit  = "1fe508be7ea0"
-	generatedDate    = "2026-05-22T11:31:07+03:00"
+	generatedVersion = "0.3.0-dev.5+gf2a415dc8bb2"
+	generatedCommit  = "f2a415dc8bb2"
+	generatedDate    = "2026-05-22T11:36:33+03:00"
 )
