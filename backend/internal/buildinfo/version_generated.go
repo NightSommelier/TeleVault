50369c32f12bd04b097c323022c1f09c25628dfe
Sommelier <Arty.03.07@gmail.com>
2026-05-22 14:51:38 +0300

    chore(version): regenerate build version

--- backend/internal/buildinfo/version_generated.go
+++ backend/internal/buildinfo/version_generated.go
@@ -1,7 +1,7 @@
 package buildinfo
 
 const (
-	generatedVersion = "0.3.0-dev.4+gef87793db109"
-	generatedCommit  = "ef87793db109"
-	generatedDate    = "2026-05-22T14:29:29+03:00"
+	generatedVersion = "0.3.0-dev.5+gec473b88daa0"
+	generatedCommit  = "ec473b88daa0"
+	generatedDate    = "2026-05-22T14:37:45+03:00"
 )
